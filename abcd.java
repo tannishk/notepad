814a84f106e31018f14f033655fea8911341b088
Tannishk Sharma <tannishk@users.noreply.github.com>
2015-11-03 20:27:45 +0530

    Create abcd.java

---
+++ abcd.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notepad;
+
+import static com.sun.awt.SecurityWarning.getSize;
+import java.awt.Dimension;
+import java.awt.Frame;
+
+/**
+ *
+ * @author Tannishk
+ */
+public class abcd {
+    static Frame f;
+    public static void main(String[] args)
+    {
+    f = new NotepadFrame("Tannishks notepad ");
+		
+		
+		
+		
+		f.setSize(800, 800);
+		
+		f.setVisible(true);
+    }
+}
